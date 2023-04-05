a92ba40b97863f4ec9729b072c4125f1a3b11788
Dahna <dahnaahn16@gmail.com>
2023-04-05 12:50:46 -0700

    make it interactive, creating a location

--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -49,6 +49,20 @@ class ConferenceDetailEncoder(ModelEncoder):
         "location": LocationListEncoder(),
     }
 
+@require_http_methods(["GET"])
+def api_list_states(request):
+    if request.method == "GET":
+        states = State.objects.order_by("name")
+        state_list = []
+        for state in states:
+            d = {
+                "name": state.name,
+                "abbreviation": state.abbreviation,
+        }
+            state_list.append(d)
+    return JsonResponse({"states": state_list})
+
+
 
 @require_http_methods(["GET", "POST"])
 def api_list_conferences(request):
